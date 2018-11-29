ca1d89d7060ef5b93f2fca023061e37157cdaf37
Arika Arnzen <arikaarnzen11@live.com>
2018-11-28 20:28:19 -0800

    Updated Register Page style

--- client/src/components/RegisterPage.jsx
+++ client/src/components/RegisterPage.jsx
@@ -3,12 +3,22 @@ import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
+import MenuAppBar from './MenuAppBar';
 import { userActions } from '../actions/user.actions';
+
+const styles = {
+  content: {
+    padding: '100px',
+  },
+  form: {
+    display: 'grid',
+  },
+};
 
 class RegisterPage extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       user: {
         firstName: '',
@@ -45,56 +55,63 @@ class RegisterPage extends React.Component {
   }
 
   render() {
-    const { registering } = this.props;
+    const { registering, classes } = this.props;
     const { user, submitted } = this.state;
     return (
       <div>
+        <MenuAppBar />
         <h2>Register</h2>
-        <form name="form" onSubmit={this.handleSubmit}>
-          <TextField
-            label="First Name"
-            margin="normal"
-            onChange={this.handleChange}
-            value={user.firstName}
-            name="firstName"
-          />
-          {submitted && !user.firstName
-                        && <div>First Name is required</div>
-                    }
-          <TextField
-            label="Last Name"
-            margin="normal"
-            onChange={this.handleChange}
-            value={user.lastName}
-            name="lastName"
-          />
-          {submitted && !user.lastName
-                        && <div>Last Name is required</div>
-                    }
-          <TextField
-            label="Email"
-            margin="normal"
-            onChange={this.handleChange}
-            value={user.username}
-            name="username"
-          />
-          {submitted && !user.username
-                        && <div>Email is required</div>
-                    }
-          <TextField
-            label="Password"
-            margin="normal"
-            onChange={this.handleChange}
-            value={user.password}
-            name="password"
-          />
-          {submitted && !user.password
-                        && <div>Password is required</div>
-                    }
-          <Button type="submit">Register</Button>
-          {registering && <CircularProgress size={20} /> }
-          <Button href="/login">Cancel</Button>
-        </form>
+        <div className={classes.content}>
+          <form className={classes.form} name="form" onSubmit={this.handleSubmit}>
+            <TextField
+              label="First Name"
+              margin="normal"
+              onChange={this.handleChange}
+              value={user.firstName}
+              name="firstName"
+            />
+            {submitted && !user.firstName
+              && <div>First Name is required</div>
+            }
+            <TextField
+              label="Last Name"
+              margin="normal"
+              onChange={this.handleChange}
+              value={user.lastName}
+              name="lastName"
+            />
+            {submitted && !user.lastName
+              && <div>Last Name is required</div>
+            }
+            <TextField
+              label="Email"
+              margin="normal"
+              onChange={this.handleChange}
+              value={user.username}
+              name="username"
+            />
+            {submitted && !user.username
+              && <div>Email is required</div>
+            }
+            <TextField
+              label="Password"
+              margin="normal"
+              onChange={this.handleChange}
+              value={user.password}
+              name="password"
+            />
+            {submitted && !user.password
+              && <div>Password is required</div>
+            }
+            <Button
+              type="submit" 
+              variant="contained"
+              color="primary"
+            >Register</Button>
+            {registering && <CircularProgress size={20} /> }
+            <Button href="/login">Cancel</Button>
+          </form>
+        </div>
       </div>
     );
   }
@@ -107,5 +124,5 @@ function mapStateToProps(state) {
   };
 }
 
-const connectedRegisterPage = connect(mapStateToProps)(RegisterPage);
+const connectedRegisterPage = connect(mapStateToProps)(withStyles(styles)(RegisterPage));
 export { connectedRegisterPage as RegisterPage };
